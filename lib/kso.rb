8312510f871120b931e80d9681a94151ad7dda15
myun2 <myun2@nwhite.info>
2013-11-27 01:26:15 +0900

    requires and hello_world ...?

---
+++ lib/kso.rb
@@ -0,0 +1,5 @@
+require "sinatra"
+require "sinatra/reloader"
+module Kso
+end
+require "kso/app"
---
+++ lib/kso/app.rb
@@ -0,0 +1,7 @@
+require "kso/renderer"
+
+module Kso
+  class Application
+    include Renderer
+  end
+end
