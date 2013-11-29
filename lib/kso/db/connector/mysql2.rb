e1b9db73d4ef5d4a13e0cb0aa067b38c387a32d0
myun2 <myun2@nwhite.info>
2013-11-29 12:20:16 +0900

    mysql2 connector implemented

--- lib/kso/db/connector/mysql2.rb
+++ lib/kso/db/connector/mysql2.rb
@@ -2,7 +2,11 @@ require 'mysql2'
 module Kso
   module Db
     module Connector
-      class Mysql2
+
+      class Mysql2 < Mysql2::Client
+        #def initialize(params)
+        #  @client = Mysql2::Client.new(params)
+        #end
       end
     end
   end
